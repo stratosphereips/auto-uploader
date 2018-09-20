51ba69e55b07e55138c680f0a818da7162024c98
Yury Kasimov <yury.kasimov@prga-005096.rad.int.avast.com>
2018-09-20 18:15:15 +0200

    add files to jin and link them to non public dataset

--- auto-uploader.sh
+++ auto-uploader.sh
@@ -41,12 +41,23 @@ for FLDR in $FOLDERS; do
     LAST_FOLDER=`ssh -p 902 project@jin ls -d /opt/Malware-Project/BigDataset/IoTScenarios/$DATASET_FOLDER_JIN* | awk -F'/' '{print $6}' | sort -V | tail -1`
     echo $FILES_IN_JIN
     NEW_INDEX=`echo $LAST_FOLDER | awk -F'-' '{print $6+1}'`
-    `ssh -p 902 project@jin mkdir /opt/Malware-Project/BigDataset/IoTScenarios/$DATASET_FOLDER_JIN-$NEW_INDEX` 
     # sort ls by name
     # Find the last number used for my folder
     # if first, empty answer. If not, get the last number
     # increment the last number
     # Create the folder in jin
+    PCAP_FILES=`ls *.pcap`
+    FOLDERS=()
+    for FILE in $PCAP_FILES; do
+       # create a new folder
+       NEW_FOLDER=`/opt/Malware-Project/BigDataset/IoTScenarios/$DATASET_FOLDER_JIN-$NEW_INDEX`
+	$FOLDERS+=($NEW_FOLDER)
+	`ssh -p 902 project@jin mkdir $NEW_FOLDER` 
+	scp -C $FILE project@jin:$NEW_FOLDER
+	scp -C $README project@jin:$NEW_FOLDER
+	ssh -p 902 project@jin ln -s $NEW_FOLDER /opt/Malware-Project/Dataset/NonPublic/$DATASET_FOLDER_JIN-$NEW_INDEX
+	NEW_INDEX=$NEW_INDEX+1
+    done 
     # cp the pcap and the README.md. If more than one pcap in local folder, a new remote folder for each of them
         # scp -C 
     # remmeber the new folders
